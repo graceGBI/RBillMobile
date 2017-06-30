8e047be264a4e38492ad5ec2c7e3d4cf66efc892
annegbi <gracebissi@yahoo.fr>
2017-07-01 01:12:03 +0200

    ecran search & company

---
+++ platforms/browser/www/js/controller/ListCompanyPageController.js
@@ -0,0 +1,4 @@
+Demo.angular.controller('ListCompanyPageController', ['$scope', '$compile', '$http', 'InitService', '$rootScope', function($scope, $compile, $http, InitService, $rootScope) {
+
+
+}])
---
+++ www/js/controller/CompanyPageController.js
@@ -0,0 +1,4 @@
+Demo.angular.controller('CompanyPageController', ['$scope', '$compile', '$http', 'InitService', '$rootScope', function($scope, $compile, $http, InitService, $rootScope) {
+
+
+}])
